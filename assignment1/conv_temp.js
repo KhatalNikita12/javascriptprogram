10c1cbb622bd8417039068eca676752215b90bb2
KhatalNikita12 <Khatalnikita2003@gmail.com>
2022-08-19 11:56:33 +0530

    convert temperature

---
+++ assignment1/conv_temp.js
@@ -0,0 +1,21 @@
+"use strict"
+ let value=1;
+switch(value)
+{
+  case 0: 
+let c;//temperature in celcius
+let f=45;//temperature in Fahrenheit
+c = ((f-32)/9)*5;
+console.log("temperature in celcius is",f,"degree");
+console.log("temperature in fahrenhit is",c,"degree");
+// }
+break;
+case 1:
+
+let c1=60;
+let f1;
+f1 = c1* 1.8 + 32;
+console.log("temperature in celcius is",c1,"degree");
+console.log("temperature in fahrenhit is",f1,"degree");
+
+}
